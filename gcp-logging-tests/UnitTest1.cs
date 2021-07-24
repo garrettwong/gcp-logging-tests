4d2a5033af18ae19293361b501505c9a187b6269
Garrett Wong <garrettwong@google.com>
2021-07-24 14:48:05 -0700

    add build

---
+++ gcp-logging-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace gcp_logging_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
