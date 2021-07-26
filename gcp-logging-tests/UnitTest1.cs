7977a8250880b60501ef8405be79d3aca9906cc0
Garrett Wong <garrettwong@google.com>
2021-07-25 22:29:18 -0700

    add test fixes

--- gcp-logging-tests/UnitTest1.cs
+++ gcp-logging-tests/UnitTest1.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace gcp_logging_tests
 {
     public class UnitTest1
     {
+        public UnitTest1()
+        {
+            if (Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS") == null)
+            {
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/Users/garrettwong/Downloads/sa-key.json");
+            }
+        }
+
+
+        public async Task<string> GetBearerToken(string aud)
+        {
+            // get bearer token
+            var oidcToken = await GoogleCredential
+                .GetApplicationDefault()
+                .GetOidcTokenAsync(OidcTokenOptions.FromTargetAudience(aud));
+            var token = await oidcToken.GetAccessTokenAsync();
+            return token;
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            var adc = await GoogleCredential
+                .GetApplicationDefaultAsync();
+            var gc = adc.CreateScoped(new string[] { "https://www.googleapis.com/auth/cloud-platform" });
+
+            var token = await gc.UnderlyingCredential.GetAccessTokenForRequestAsync();
+            
+            return token;
+        }
+
         [Fact]
         public void Test1()
         {
@@ -25,16 +58,6 @@ namespace gcp_logging_tests
         }
 
 
-
-        public async Task<string> GetBearerToken(string aud)
-        {
-            // get bearer token
-            var oidcToken = await GoogleCredential
-                .GetApplicationDefault()
-                .GetOidcTokenAsync(OidcTokenOptions.FromTargetAudience(aud));
-            var token = await oidcToken.GetAccessTokenAsync();
-            return token;
-        }
 
         [Fact]
         public async Task FunctionsCall()
@@ -57,15 +80,51 @@ namespace gcp_logging_tests
         [Fact]
         public async Task BucketsGet()
         {
-            var functionUrl = "https://storage.googleapis.com/storage/v1/b?project=gwc-sandbox";
+            var token = await GetAccessToken();
 
-            var token = await GetBearerToken(functionUrl);
+            // generate at
+            var url = "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/csharp-on-gcp%40gwc-service-accounts.iam.gserviceaccount.com:generateAccessToken";
 
+            // IAM Creds
             using var client = new HttpClient();
             client.Timeout = TimeSpan.FromSeconds(10);
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
 
-            var r = await client.GetStringAsync(functionUrl);
+            var scopes = new {
+                scope = new string[] { "https://www.googleapis.com/auth/cloud-platform" }
+            };
+            var json = JsonConvert.SerializeObject(scopes);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            
+            var r = await client.PostAsync(url, data);
+
+            var s = r.Content.ReadAsStringAsync();
+            var o = JsonConvert.DeserializeObject<dynamic>(s.Result);
+            var accessToken = o.accessToken.ToString();
+            
+
+            // API Client Lib Call
+            var gc = GoogleCredential.GetApplicationDefault();
+            var sc = StorageClient.Create(gc);
+            var projectId = "gwc-sandbox";
+            var buckets = sc.ListBuckets(projectId);
+            foreach (var bucket in buckets)
+            {
+                Console.WriteLine(bucket.Name);
+            }
+
+
+            // Get Buckets API Call
+            var storageUrl = "https://storage.googleapis.com/storage/v1/b?project=gwc-sandbox";
+
+            using var client2 = new HttpClient();
+            client2.Timeout = TimeSpan.FromSeconds(10);
+            client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var r2 = await client2.GetStringAsync(storageUrl);
 
             Console.WriteLine(r);
             Assert.NotNull(r);
