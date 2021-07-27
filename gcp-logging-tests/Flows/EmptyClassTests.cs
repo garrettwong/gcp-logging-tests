effe7fcfefbf4395b2bd88a37df25d85c570c71e
Garrett Wong <garrettwong@google.com>
2021-07-27 12:58:51 -0700

    add flows

---
+++ gcp-logging-tests/Flows/EmptyClassTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace gcp_logging_tests.Flows
+{
+    public class EmptyClassTests
+    {
+        public EmptyClassTests()
+        {
+            if (Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS") == null)
+            {
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/Users/garrettwong/Downloads/sa-key.json");
+            }
+        }
+
+
+        [Fact]
+        public void ApplicationDefaultCredentails()
+        {
+            var credentialsPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+
+            Assert.NotNull(credentialsPath);
+        }
+
+    }
+}
