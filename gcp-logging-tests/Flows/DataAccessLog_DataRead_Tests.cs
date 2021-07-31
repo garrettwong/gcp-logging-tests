d8103626280ea30a8459f716341bcd24cd05a813
Garrett Wong <garrettwong@google.com>
2021-07-30 22:01:01 -0700

    add data access write tests

--- gcp-logging-tests/Flows/DataAccessLog_DataRead_Tests.cs
+++ gcp-logging-tests/Flows/DataAccessLog_DataRead_Tests.cs
@@ -75,7 +75,6 @@ namespace gcp_logging_tests.Flows
                 _gcpLogQueryGenerator.GetDataAccessLogQuery(projectId, serviceName, methodName, 5)
             ).Count();
 
-
             // Write Data
             var storage = new Storage();
             var res = storage.ReadObject(projectId, bucketName, objectName);
