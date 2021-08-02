4a878636a4f6dfb4ff8c7827707a0a3c63f293a9
Garrett Wong <garrettwong@google.com>
2021-08-01 21:25:42 -0700

    add bq transfer ex

--- terraform/init.sh
+++ terraform/init.sh
@@ -68,7 +68,7 @@ gcloud beta compute \
 bq mk admin_logs
 gcloud logging sinks create admin_logs \
     "bigquery.googleapis.com/projects/${PROJECT_ID}/datasets/admin_logs" \
-    --log-filter='logName="projects/gwc-sandbox/logs/cloudaudit.googleapis.com%2Factivity"'
+    --log-filter='logName="projects/'${PROJECT_ID}'/logs/cloudaudit.googleapis.com%2Factivity"'
 WRITER_IDENTITY=$(gcloud logging sinks describe admin_logs --format="value(writerIdentity)")
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="$WRITER_IDENTITY" \
@@ -77,21 +77,18 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 bq mk data_access
 gcloud logging sinks create data_access \
     "bigquery.googleapis.com/projects/${PROJECT_ID}/datasets/data_access" \
-    --log-filter='logName="projects/gwc-sandbox/logs/cloudaudit.googleapis.com%2Fdata_access"'
+    --log-filter='logName="projects/'${PROJECT_ID}'/logs/cloudaudit.googleapis.com%2Fdata_access"'
 WRITER_IDENTITY=$(gcloud logging sinks describe data_access --format="value(writerIdentity)")
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="$WRITER_IDENTITY" \
     --role=roles/bigquery.dataEditor
 
 # SINK to BIGQUERY for VPC Flows
-bq mk vpc_flows2
-
+bq mk vpc_flows
 gcloud logging sinks create vpc_flows \
-    "bigquery.googleapis.com/projects/${PROJECT_ID}/datasets/vpc_flows2" \
+    "bigquery.googleapis.com/projects/${PROJECT_ID}/datasets/vpc_flows" \
     --log-filter='resource.type="gce_subnetwork"'
-
 WRITER_IDENTITY=$(gcloud logging sinks describe vpc_flows --format="value(writerIdentity)")
-
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="$WRITER_IDENTITY" \
     --role=roles/bigquery.dataEditor
@@ -100,7 +97,7 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 bq mk firewall
 gcloud logging sinks create firewall \
     "bigquery.googleapis.com/projects/${PROJECT_ID}/datasets/firewall" \
-    --log-filter='logName="projects/gwc-sandbox/logs/compute.googleapis.com%2Ffirewall"'
+    --log-filter='logName="projects/'${PROJECT_ID}'/logs/compute.googleapis.com%2Ffirewall"'
 WRITER_IDENTITY=$(gcloud logging sinks describe firewall --format="value(writerIdentity)")
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="$WRITER_IDENTITY" \
@@ -126,6 +123,19 @@ gcloud beta compute \
     --shielded-integrity-monitoring \
     --reservation-affinity=any
 
-# curl many
-for i in {1..5000}; do curl -H 'Cache-Control: no-cache' "http://34.134.129.29?i=$i" ; done
-for i in {1..2500}; do curl -H 'Cache-Control: no-cache' "http://10.128.0.23?i=$i" ; done
+# generate vpc flow logs
+for i in {1..5000}; do 
+    curl -H 'Cache-Control: no-cache' "http://34.134.129.29?i=$i"; 
+done
+
+# generate vpc flow logs - from internal ip
+for i in {1..2500}; do 
+    curl -H 'Cache-Control: no-cache' "http://10.128.0.23?i=$i"; 
+done
+
+# generate firewall logs
+for i in {1..55000}; do 
+    port="$(($i + 10000))"
+    echo $port
+    curl -m 0.5 "34.134.129.29:${port}"; 
+done
